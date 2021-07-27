2700de814575057d781853f7ac8851e360d643a4
carmichaeljr <carmichaeljr@outlook.com>
2021-07-26 23:02:35 -0600

    Execute test functional

--- src/AutoGrader/ExecuteTest.java
+++ src/AutoGrader/ExecuteTest.java
@@ -1,11 +1,14 @@
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 import com.google.gson.annotations.SerializedName;
 
 class ExecuteTest extends Test {
 	@SerializedName("removeWhitespace")
 	private boolean removeWhiteSpace;
+	@SerializedName("matchCase")
+	private boolean matchCase;
 	@SerializedName("outputConditional")
 	private String outputConditional;
 	@SerializedName("input")
@@ -29,9 +32,9 @@ class ExecuteTest extends Test {
 
 	@Override
 	public Pair<Float,String> run(RarsProcManager rarsProcRef, String student){
-		String rv=this.runProgram(rarsProcRef,super.getTestFile(student));
-		Pair<Float,String> rv=this.checkForErrorCodes(rv);
-		rv=(rv!=null)? this.checkForOutputs(rv): rv;
+		String progOutput=this.runProgram(rarsProcRef,super.getTestFile(student));
+		Pair<Float,String> rv=this.checkForErrorCodes(progOutput);
+		rv=(rv==null)? this.checkForOutputs(progOutput): rv;
 		return rv;
 	}
 
@@ -42,20 +45,67 @@ class ExecuteTest extends Test {
 			//TODO - check for grading script file
 			files.add(submissionFile);
 			rv=rarsProcRef.pipe(files,this.inputs,this.regVals.keySet());
-			System.out.println(rv);
+			//System.out.println(rv);
 		}
 		return rv;
 	}
 
 	private Pair<Float,String> checkForErrorCodes(String progOutput){
-		if (rv.contains(ExecuteTest.ASSEMBLY_ERROR)){
+		if (progOutput.contains(ExecuteTest.ASSEMBLY_ERROR)){
 			return new Pair<Float,String>((float)0.0,"Code did not assemble.");
-		} else if (rv.contains(ExecuteTest.SIMULATION_ERROR)){
+		} else if (progOutput.contains(ExecuteTest.SIMULATION_ERROR)){
 			return new Pair<Float,String>((float)0.0,"Code had an error during execution.");
-		} else if (rv.contains(ExecuteTest.INFINITE_LOOP)){
+		} else if (progOutput.contains(ExecuteTest.INFINITE_LOOP)){
 			return new Pair<Float,String>((float)0.0,"Code had an infinite loop during execution.");
 		}
 		return null;
 	}
-}
 
+	private Pair<Float,String> checkForOutputs(String progOutput){
+		if (this.removeWhiteSpace){	
+			progOutput.replaceAll("\\s+","");
+		}
+		if (this.matchCase){
+			progOutput.toLowerCase();
+		}
+		boolean someOutputs=this.outputConditional.equalsIgnoreCase("or");
+		boolean result=!someOutputs;
+		for (int i=0; i<this.outputs.size(); i++){
+			String expected=(this.matchCase)? this.outputs.get(i): this.outputs.get(i).toLowerCase();
+			boolean iterResult=(someOutputs)? progOutput.contains(expected): !progOutput.contains(expected);
+			if (iterResult){
+				result=(someOutputs)? (iterResult && true): (iterResult && false);
+			}
+		}
+		if (result){
+			return new Pair<Float,String>(super.points,"");
+		}
+		return new Pair<Float,String>((float)0.0,this.formatComment());
+	}
+
+	private String formatComment(){
+		StringBuilder builder=new StringBuilder("Program did not output ");
+		this.generateMessagePart(builder,this.outputs,this.outputConditional);
+		builder.append(" given inputs of ");
+		this.generateMessagePart(builder,this.inputs,"and");
+		builder.append(".");
+		return builder.toString();
+	}
+
+	private void generateMessagePart(StringBuilder builder, ArrayList<String> vals, String conditional){
+		for (int i=0; i<vals.size(); i++){
+			if (vals.get(i).equalsIgnoreCase("\n")){
+				builder.append("\\n");
+			} else {
+				builder.append(vals.get(i));
+			}
+			if (i==vals.size()-2){
+				builder.append(", ");
+				builder.append(conditional);
+				builder.append(" ");
+			} else if (i<vals.size()-2){
+				builder.append(", ");
+			}
+		}
+	}
+}
